ca288c11e0735ec3b4f3dc512677edcde40bac90
Lukas Leonard Köning <llkoening@posteo.de>
2020-04-29 15:26:31 +0200

    Implemented time formatting

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,10 @@
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds [[maybe_unused]]) { return string(); }
+// Converts long int of seconds into string of format hh:mm:ss
+string Format::ElapsedTime(long seconds) {
+  long int minutes = seconds / 60;
+  long int hours = minutes / 60;
+  return std::to_string(hours) + ":" + std::to_string(minutes % 60) + ":" +
+         std::to_string(seconds % 60);
+}
