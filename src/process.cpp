af222d84d814c65a476420250bf44c1619814b7e
Lukas Leonard Köning <llkoening@posteo.de>
2020-04-30 11:30:54 +0200

    Improved variables

--- src/process.cpp
+++ src/process.cpp
@@ -26,16 +26,17 @@ float Process::getCpuUtilization() const { return cpu_utilization; }
 void Process::updateCpuUtilization() {
   // Get raw data
   vector<string> cpu_usage = LinuxParser::ProcessCpuUtilization(this->pid);
-  long int utime = std::stol(cpu_usage[0]);
-  long int stime = std::stol(cpu_usage[1]);
-  long int cutime = std::stol(cpu_usage[2]);
-  long int cstime = std::stol(cpu_usage[3]);
-  long int starttime = std::stol(cpu_usage[4]);
-  long int systime = LinuxParser::UpTime();
+  const long int utime = std::stol(cpu_usage[0]);
+  const long int stime = std::stol(cpu_usage[1]);
+  const long int cutime = std::stol(cpu_usage[2]);
+  const long int cstime = std::stol(cpu_usage[3]);
+  const long int starttime = std::stol(cpu_usage[4]);
+  const long int systime = LinuxParser::UpTime();
 
   // Calculate cpu ustilization
-  double TOTAL_TIME = (utime + stime + cutime + cstime) / sysconf(_SC_CLK_TCK);
-  double ELAPSED_TIME = systime - (starttime / sysconf(_SC_CLK_TCK));
+  const float TOTAL_TIME =
+      (utime + stime + cutime + cstime) / sysconf(_SC_CLK_TCK);
+  const float ELAPSED_TIME = systime - (starttime / sysconf(_SC_CLK_TCK));
   cpu_utilization = TOTAL_TIME / ELAPSED_TIME;
 }
 
--- src/processor.cpp
+++ src/processor.cpp
@@ -10,19 +10,20 @@ using std::vector;
 // Returns the aggregate CPU utilization
 float Processor::Utilization() {
   vector<string> cpu_usage = LinuxParser::CpuUtilization();
-  long int usertime = std::stol(cpu_usage[0]);
-  long int nicetime = std::stol(cpu_usage[1]);
-  long int systemtime = std::stol(cpu_usage[2]);
-  long int idletime = std::stol(cpu_usage[3]);
-  long int iowait = std::stol(cpu_usage[4]);
-  long int irq = std::stol(cpu_usage[5]);
-  long int softirq = std::stol(cpu_usage[6]);
-  long int steal = std::stol(cpu_usage[7]);
+  const long int usertime = std::stol(cpu_usage[0]);
+  const long int nicetime = std::stol(cpu_usage[1]);
+  const long int systemtime = std::stol(cpu_usage[2]);
+  const long int idletime = std::stol(cpu_usage[3]);
+  const long int iowait = std::stol(cpu_usage[4]);
+  const long int irq = std::stol(cpu_usage[5]);
+  const long int softirq = std::stol(cpu_usage[6]);
+  const long int steal = std::stol(cpu_usage[7]);
 
   // Calculate needed jiffy values (current state)
-  float IDLE = idletime + iowait;
-  float NON_IDLE = usertime + nicetime + systemtime + irq + softirq + steal;
-  float TOTAL = IDLE + NON_IDLE;
+  const float IDLE = idletime + iowait;
+  const float NON_IDLE =
+      usertime + nicetime + systemtime + irq + softirq + steal;
+  const float TOTAL = IDLE + NON_IDLE;
 
   if (prevuser == -1 || prevnice == -1 || prevsystem == -1 || previdle == -1 ||
       previowait == -1 || previrq == -1 || prevsoftirq == -1 ||
@@ -41,14 +42,14 @@ float Processor::Utilization() {
     return (TOTAL - IDLE) / TOTAL;
   } else {
     // Calculate needed jiffy values (previous state)
-    long int PREV_IDLE = previdle + previowait;
-    long int PREV_NON_IDLE =
+    const long int PREV_IDLE = previdle + previowait;
+    const long int PREV_NON_IDLE =
         prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal;
-    long int PREV_TOTAL = PREV_IDLE + PREV_NON_IDLE;
+    const long int PREV_TOTAL = PREV_IDLE + PREV_NON_IDLE;
 
     // Calculate differential values
-    double TOTAL_DIF = TOTAL - PREV_TOTAL;
-    double IDLE_DIF = IDLE - PREV_IDLE;
+    const float TOTAL_DIF = TOTAL - PREV_TOTAL;
+    const float IDLE_DIF = IDLE - PREV_IDLE;
 
     // Save current CPU status for future calculations
     prevuser = usertime;
