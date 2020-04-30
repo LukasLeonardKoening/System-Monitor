c91fe918aea74a0ef51a119def23c823cecf14da
Lukas Leonard Köning <llkoening@posteo.de>
2020-04-30 11:48:14 +0200

    Improved time format

--- src/format.cpp
+++ src/format.cpp
@@ -8,6 +8,24 @@ using std::string;
 string Format::ElapsedTime(long seconds) {
   long int minutes = seconds / 60;
   long int hours = minutes / 60;
-  return std::to_string(hours) + ":" + std::to_string(minutes % 60) + ":" +
-         std::to_string(seconds % 60);
+
+  string hours_s, minutes_s, seconds_s;
+
+  if (hours < 10) {
+    hours_s = "0" + std::to_string(hours);
+  } else {
+    hours_s = std::to_string(hours);
+  }
+  if ((minutes % 60) < 10) {
+    minutes_s = "0" + std::to_string(minutes % 60);
+  } else {
+    minutes_s = std::to_string(minutes % 60);
+  }
+  if ((seconds % 60) < 10) {
+    seconds_s = "0" + std::to_string(seconds % 60);
+  } else {
+    seconds_s = std::to_string(seconds % 60);
+  }
+
+  return hours_s + ":" + minutes_s + ":" + seconds_s;
 }
